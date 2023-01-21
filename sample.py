b67f811aa5da46942615c9fea89a15be612475e6
박짱돌 <superstone@bagjjangdol-ui-MacBookAir.local>
2023-01-21 22:17:41 +0900

    add skeletone train, sample code

---
+++ sample.py
@@ -0,0 +1,9 @@
+"""
+Easy sampling via DDPM model example.
+"""
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -2,9 +2,33 @@ import torch
 from model import UNet
 
 def main():
+    # load model
     model = UNet()
     ddpm = DiffusionModel(model = model,
                           num_timesteps=1000)
+    
+    # load data
+    train_dataset = None
+    train_dataloader = None
+    test_dataset = None
+    test_dataloader = None
+
+    # load optimizer, scheduler
+    optimzer = None
+    scheduler = None
+
+    for epoch in range(5):
+        # train
+        for batch_idx, batch in enumerate(train_dataloader):
+            loss = ddpm(batch)
+            loss.backward()
+            optimzer.step()
+            optimzer.zero_grad()
+        # eval
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(test_dataloader):
+                loss = ddpm(batch)
+    
 
 if __name__ == "__main__":
     main()
