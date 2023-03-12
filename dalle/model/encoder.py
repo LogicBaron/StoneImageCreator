4e381309470b9c054e379f077c4b1752347a2501
suuperstone <jhjh9701@gmail.com>
2023-03-12 20:03:56 +0900

    update

--- dalle/model/encoder.py
+++ dalle/model/encoder.py
@@ -1,13 +1,21 @@
 import torch
 from torch import nn
 
+class Vae(nn.Module):
+    def __init__(self, in_dim, out_dim, hidden_dim=64, n_layers):
+        self.input_conv = nn.Conv2d(in_dim, hidden_dim, kernelt_size=(3,3), padding=(1,1))
+        self.group1 = nn.Sequential(OrderedDict([
+            'block_0', VaeBlock(hidden_dim, hidden_dim, )
+        ]))
+
+
 class VaeBlock(nn.Module):
     def __init__(self, in_dim: int, out_dim: int, hidden_dim: int, n_layers: int):
         super().__init__()
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.hidden_dim  = hidden_dim
-
+        self.post_gain = 1 / (self.n_layers ** 2)
         self.convs = nn.ModuleList([])
         in_c = in_dim
         out_c = hidden_dim
@@ -24,7 +32,7 @@ class VaeBlock(nn.Module):
         h = x
         for conv in self.convs:
             h = conv(x)
-        return self.res(x) + h
+        return self.res(x) * self.post_gain + h
 
 class ConvBlock(nn.Module):
     def __init__(self, in_dim, out_dim):
